4cd6bc7edbf1b2ee02f341f6ff9e18c1c14604f7
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-01 18:02:58 -0900

    Adicionando mocha no teste do gerador
    Teste falha.

--- 08-acoplamento/test/gerador_de_nota_fiscal_test.rb
+++ 08-acoplamento/test/gerador_de_nota_fiscal_test.rb
@@ -1,15 +1,19 @@
 #encoding: UTF-8
 require 'test/unit'
-require File.expand_path('../lib/pedido', File.dirname(__FILE__))
-require File.expand_path('../lib/gerador_de_nota_fiscal', File.dirname(__FILE__))
+require 'mocha/setup'
+require File.expand_path('../lib/pedido.rb', File.dirname(__FILE__))
+require File.expand_path('../lib/gerador_de_nota_fiscal.rb', File.dirname(__FILE__))
 
 class GeradorDeNotaFiscalTest < Test::Unit::TestCase
-  def teste_deve_gerar_NF_com_valor_de_imposto_descontado
+  def teste_deve_persistir_NF_gerada
+    # criando o mock
+    dao = mock
+    # marcando o que esperamos que aconteça
+    dao.expects(:persiste)
+
     gerador = GeradorDeNotaFiscal.new
     pedido = Pedido.new('Mauricio', 1000, 1)
 
     nf = gerador.gera pedido
-
-    assert_equal 1000 * 0.94, nf.valor
   end
 end
