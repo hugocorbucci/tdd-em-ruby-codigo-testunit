8b5cc27d95e0fa2f59a3a3d0a70608e7a336e5c9
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-02 09:38:27 -0900

    Passando teste de boleto único.
    Teve que expor mais da fatura.

--- 09-encapsulamento/lib/processador_de_boletos.rb
+++ 09-encapsulamento/lib/processador_de_boletos.rb
@@ -1,13 +1,17 @@
 #encoding: UTF-8
-require File.expand_path('./pagamento.rb',
-  File.dirname(__FILE__))
+require File.expand_path('./pagamento.rb', File.dirname(__FILE__))
 
 class ProcessadorDeBoletos
   def processa boletos, fatura
-    boleto = boletos[0]
-    pagamento = 
-      Pagamento.new boleto.valor, MeioDePagamento::BOLETO
-    fatura.pagamentos << pagamento
+    valor_total = 0
+    boletos.each do |boleto|
+      pagamento = Pagamento.new boleto.valor, MeioDePagamento::BOLETO
+      fatura.pagamentos << pagamento
+
+      valor_total += boleto.valor
+    end
+
+    fatura.paga = true if valor_total >= fatura.valor
   end
 end
 
