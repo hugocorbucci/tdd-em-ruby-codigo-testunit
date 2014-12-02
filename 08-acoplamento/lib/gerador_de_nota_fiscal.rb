d070d313a891bfdbc0861de9dcdfac2531b9f233
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-01 18:26:51 -0900

    Adicionando relogio do sistema

--- 08-acoplamento/lib/gerador_de_nota_fiscal.rb
+++ 08-acoplamento/lib/gerador_de_nota_fiscal.rb
@@ -1,12 +1,15 @@
 #encoding: UTF-8
 require File.expand_path('./nota_fiscal.rb', File.dirname(__FILE__))
+require File.expand_path('./relogio_do_sistema.rb', File.dirname(__FILE__))
 class GeradorDeNotaFiscal
-  def initialize(acoes)
+  def initialize(acoes, relogio = RelogioDoSistema.new)
     @acoes = acoes
+    @relogio = relogio
   end
   def gera(pedido)
     nf = NotaFiscal.new pedido.cliente,
-      pedido.valor_total * 0.94, Time.now
+      pedido.valor_total * 0.94,
+      @relogio.hoje
 
     @acoes.each { |acao| acao.executa nf }
 
