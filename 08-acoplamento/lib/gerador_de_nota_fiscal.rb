43a6e3045862bf3dd3146b1e598b075fa75ea4bf
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-01 18:23:00 -0900

    Refatorando para receber acoes genéricas
    Note que o NFDao e o SAP

--- 08-acoplamento/lib/gerador_de_nota_fiscal.rb
+++ 08-acoplamento/lib/gerador_de_nota_fiscal.rb
@@ -1,16 +1,14 @@
 #encoding: UTF-8
 require File.expand_path('./nota_fiscal.rb', File.dirname(__FILE__))
 class GeradorDeNotaFiscal
-  def initialize(dao, sap)
-    @dao = dao
-    @sap = sap
+  def initialize(acoes)
+    @acoes = acoes
   end
   def gera(pedido)
     nf = NotaFiscal.new pedido.cliente,
       pedido.valor_total * 0.94, Time.now
 
-    @dao.persiste nf
-    @sap.envia nf
+    @acoes.each { |acao| acao.executa nf }
 
     nf
   end
--- 08-acoplamento/test/gerador_de_nota_fiscal_test.rb
+++ 08-acoplamento/test/gerador_de_nota_fiscal_test.rb
@@ -5,21 +5,11 @@ require File.expand_path('../lib/pedido.rb', File.dirname(__FILE__))
 require File.expand_path('../lib/gerador_de_nota_fiscal.rb', File.dirname(__FILE__))
 
 class GeradorDeNotaFiscalTest < Test::Unit::TestCase
-  def teste_deve_persistir_NF_gerada
-    dao = mock
-    dao.expects(:persiste)
-    sap = stub_everything
-    
-    gerador = GeradorDeNotaFiscal.new dao, sap
-    pedido = Pedido.new('Mauricio', 1000, 1)
+  def teste_deve_invocar_acoes_posteriores
+    acao1 = mock.tap{|dao| dao.expects(:executa)}
+    acao2 = mock.tap{|sap| sap.expects(:executa)}
 
-    nf = gerador.gera pedido
-  end
-  def teste_deve_enviar_NF_gerada_para_SAP
-    dao = stub_everything
-    sap = mock.tap{|sap| sap.expects(:envia)}
-
-    gerador = GeradorDeNotaFiscal.new dao, sap
+    gerador = GeradorDeNotaFiscal.new [acao1, acao2]
     pedido = Pedido.new('Mauricio', 1000, 1)
 
     nf = gerador.gera pedido
