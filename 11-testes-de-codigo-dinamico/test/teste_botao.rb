5bb05217d837040845753fff549839c8653b8508
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-02 10:46:50 -0900

    Teste e implementacao que usam lambdas

--- 11-testes-de-codigo-dinamico/test/teste_botao.rb
+++ 11-testes-de-codigo-dinamico/test/teste_botao.rb
@@ -4,15 +4,17 @@ require 'mocha/setup'
 require File.expand_path('../lib/botao.rb', File.dirname(__FILE__))
 
 class TesteBotao < Test::Unit::TestCase
-  def teste_chama_metodo_no_alvo_ao_executar
-    botao = Botao.new(:teste)
+  def teste_chama_metodo_no_alvo
+    botao = Botao.new(lambda{|alvo| alvo.teste})
     alvo = mock
     alvo.expects(:teste).with()
 
     botao.executa_em(alvo)
   end
   def teste_chama_metodo_no_alvo_com_parametros_ao_executar
-    botao = Botao.new(:teste, :parametro1, [:parametro2])
+    botao = Botao.new(lambda{ |alvo|
+      alvo.teste(:parametro1, [:parametro2])
+    })
     alvo = mock
     alvo.expects(:teste).with(:parametro1, [:parametro2])
 
