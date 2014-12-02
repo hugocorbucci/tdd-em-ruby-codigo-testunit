9733ffd744fa3741331ffd540fe47e1a914ece8e
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-01 15:29:39 -0900

    Teste que falha na seção 2.3

--- 02-testes-de-unidade/lib/testa_maior_e_menor.rb
+++ 02-testes-de-unidade/lib/testa_maior_e_menor.rb
@@ -4,8 +4,8 @@ require File.expand_path(File.join(File.dirname(__FILE__), './carrinho_de_compra
 require File.expand_path(File.join(File.dirname(__FILE__), './maior_e_menor.rb'))
 
 carrinho = CarrinhoDeCompras.new
-carrinho << Produto.new('Liquidificador', 250.0)
 carrinho << Produto.new('Geladeira', 450.0)
+carrinho << Produto.new('Liquidificador', 250.0)
 carrinho << Produto.new('Jogo de pratos', 70.0)
 
 algoritmo = MaiorEMenor.new
---
+++ 02-testes-de-unidade/test/teste_maior_e_menor.rb
@@ -0,0 +1,21 @@
+#encoding: UTF-8
+require 'test/unit'
+require File.expand_path(File.join(File.dirname(__FILE__), '../lib/maior_e_menor.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), '../lib/carrinho_de_compras.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), '../lib/produto.rb'))
+
+class TesteMaiorMenor < Test::Unit::TestCase
+  def test_ordem_descrescente
+    carrinho = CarrinhoDeCompras.new
+
+    carrinho << Produto.new('Geladeira', 450.0)
+    carrinho << Produto.new('Liquidificador', 250.0)
+    carrinho << Produto.new('Jogo de pratos', 70.0)
+
+    algoritmo = MaiorEMenor.new
+    algoritmo.encontra carrinho
+    
+    assert_equal 'Jogo de pratos', algoritmo.menor.nome
+    assert_equal 'Geladeira', algoritmo.maior.nome
+  end
+end
