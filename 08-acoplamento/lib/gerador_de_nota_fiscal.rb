41433d4b455da87491ef8c118a8ce7ecf914cafd
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-01 18:12:18 -0900

    Adicionando teste para SAP e passando o teste

--- 08-acoplamento/lib/gerador_de_nota_fiscal.rb
+++ 08-acoplamento/lib/gerador_de_nota_fiscal.rb
@@ -1,14 +1,16 @@
 #encoding: UTF-8
 require File.expand_path('./nota_fiscal.rb', File.dirname(__FILE__))
 class GeradorDeNotaFiscal
-  def initialize(dao)
+  def initialize(dao, sap)
     @dao = dao
+    @sap = sap
   end
   def gera(pedido)
     nf = NotaFiscal.new pedido.cliente,
       pedido.valor_total * 0.94, Time.now
 
     @dao.persiste nf
+    @sap.envia nf
 
     nf
   end
