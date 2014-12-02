f0b0c0bee2ecb2ae457694fedf0b8ca12c88155c
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-02 09:29:32 -0900

    Primeiro teste do processador de boletos

---
+++ 09-encapsulamento/test/processador_de_boletos_test.rb
@@ -0,0 +1,19 @@
+#encoding: UTF-8
+require 'test/unit'
+require File.expand_path('../lib/processador_de_boletos.rb', File.dirname(__FILE__))
+require File.expand_path('../lib/fatura.rb', File.dirname(__FILE__))
+require File.expand_path('../lib/boleto.rb', File.dirname(__FILE__))
+
+class ProcessadorDeBoletosTest < Test::Unit::TestCase
+  def teste_deve_processar_pagamento_via_boleto_unico
+    processador = ProcessadorDeBoletos.new
+
+    fatura = Fatura.new('Cliente', 150.0)
+    b1 = Boleto.new(150.0)
+
+    processador.processa([b1], fatura)
+
+    assert_equal 1, fatura.pagamentos.size
+    assert_equal 150.0, fatura.pagamentos[0].valor
+  end
+end
