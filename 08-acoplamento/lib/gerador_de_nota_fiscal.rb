fba1ea41bf0bd6479edaf05684cf4fb387e8a950
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-01 18:35:26 -0900

    Adicionando tabela e passando o teste
    Último código do capítulo 8

--- 08-acoplamento/lib/gerador_de_nota_fiscal.rb
+++ 08-acoplamento/lib/gerador_de_nota_fiscal.rb
@@ -1,14 +1,17 @@
 #encoding: UTF-8
 require File.expand_path('./nota_fiscal.rb', File.dirname(__FILE__))
 require File.expand_path('./relogio_do_sistema.rb', File.dirname(__FILE__))
+require File.expand_path('./tabela.rb', File.dirname(__FILE__))
 class GeradorDeNotaFiscal
-  def initialize(acoes, relogio = RelogioDoSistema.new)
+  def initialize(acoes, relogio = RelogioDoSistema.new,
+    tabela = Tabela.new)
     @acoes = acoes
     @relogio = relogio
+    @tabela = tabela
   end
   def gera(pedido)
     nf = NotaFiscal.new pedido.cliente,
-      pedido.valor_total * 0.94,
+      pedido.valor_total * @tabela[pedido.valor_total],
       @relogio.hoje
 
     @acoes.each { |acao| acao.executa nf }
