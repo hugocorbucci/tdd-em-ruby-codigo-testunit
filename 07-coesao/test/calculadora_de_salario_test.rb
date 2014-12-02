8423e1ea9e07be75da42c9fd84109b7c2ca72d69
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-01 17:12:16 -0900

    Implementação de base para capítulo 7

---
+++ 07-coesao/test/calculadora_de_salario_test.rb
@@ -0,0 +1,23 @@
+#encoding: UTF-8
+require 'test/unit'
+require File.expand_path('../lib/calculadora_de_salario.rb', File.dirname(__FILE__))
+require File.expand_path('../lib/funcionario.rb', File.dirname(__FILE__))
+
+class CalculadoraDeSalarioTest < Test::Unit::TestCase
+  def teste_deve_calcular_salario_para_desenvolvedores_com_salario_abaixo_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Mauricio', 1500.0, Cargo::DESENVOLVEDOR)
+
+    salario = calculadora.calcula_salario desenvolvedor
+
+    assert_equal 1500 * 0.9, salario
+  end
+  def teste_deve_calcular_salario_para_desenvolvedores_com_salario_acima_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Mauricio', 4000.0, Cargo::DESENVOLVEDOR)
+
+    salario = calculadora.calcula_salario desenvolvedor
+
+    assert_equal 4000 * 0.8, salario
+  end
+end
